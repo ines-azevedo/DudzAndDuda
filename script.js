a8b512f1b726f6ab4b7f114d7b59b3fc33330b93
ines-azevedo <isamoreira2001@gmail.com>
2022-12-30 22:25:18 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+
+function showfato() {
+        /*mostrar*/
+        document.getElementById('dudzfato_img').style.display = "block"
+        document.getElementById('text2').style.display = "block";
+        document.getElementById('images2').style.display = "block";
+        /*apagar*/
+        document.getElementById('1_elementos').style.display = "none";
+        document.getElementById('dudz-image').style.display = "none";
+        document.getElementById('p1').style.display = "none";
+}
+
+
+function showvestido() {
+        /*mostrar*/
+        document.getElementById('dudzvestido_img').style.display = "block"
+        document.getElementById('text2').style.display = "block";
+        document.getElementById('images2').style.display = "block";
+        /*apagar*/
+        document.getElementById('1_elementos').style.display = "none";
+        document.getElementById('dudz-image').style.display = "none";
+        document.getElementById('p1').style.display = "none";
+}
+function showpato() {
+        /*mostrar*/
+        document.getElementById('dudzpato_img').style.display = "block"
+        document.getElementById('text2').style.display = "block";
+        document.getElementById('images2').style.display = "block";
+        /*apagar*/
+        document.getElementById('1_elementos').style.display = "none";
+        document.getElementById('dudz-image').style.display = "none";
+        document.getElementById('p1').style.display = "none";
+}
+function showsweat() {
+        /*mostrar*/
+        document.getElementById('dudzsweat_img').style.display = "block"
+        document.getElementById('text2').style.display = "block";
+        document.getElementById('images2').style.display = "block";
+        /*apagar*/
+        document.getElementById('1_elementos').style.display = "none";
+        document.getElementById('dudz-image').style.display = "none";
+        document.getElementById('p1').style.display = "none";
+}
+function showfinal() {
+        /*mostrar*/
+        document.getElementById('cara_img').style.display = "block";
+        document.getElementById('text3').style.display = "block";
+        document.getElementById('images3').style.display = "block";
+        /*apagar*/
+        document.getElementById('images2').style.display = "none";
+        document.getElementById('p2').style.display = "none";
+}
+
+function back() {
+        window.location.reload();
+}
